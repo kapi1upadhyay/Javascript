f0edd72ae2252ad553240aff84e602964fed410b
kapil upadhyay <87381213+kapi1upadhyay@users.noreply.github.com>
2024-05-04 16:39:24 +0530

    Basic.js

---
+++ Basic.js
@@ -0,0 +1,30 @@
+console.log("Hello world!");
+
+let str="kapil"
+let str1="upadhyay"
+
+console.log(`my name is ${str1}`)
+
+// character position
+console.log(str.charAt(2));
+
+//index of character
+console.log(str1.indexOf('p'))
+
+const newstring="  school  ";
+console.log(newstring.trim())
+
+const num=99.609;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3))
+
+//math fuction
+console.log(Math.abs(-4))
+console.log(Math.random())
+
+//Date and Time 
+const date=new Date()
+console.log(date.toString())
+console.log(date.toLocaleString())
+console.log(date.toLocaleDateString())
+console.log(date.getFullYear())
